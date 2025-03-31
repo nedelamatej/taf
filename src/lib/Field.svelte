0c895f664ce0cd921a918677a48722cf2d68cb55
Matěj Neděla <nedela.matej@gmail.com>
2025-03-31 20:38:16 +0200

    feat: add `Field` component

---
+++ src/lib/Field.svelte
@@ -0,0 +1,46 @@
+<script>
+  /*
+   * Bakalarska prace
+   * MODELOVANI A ANALYZA TRAJEKTORII SOFTBALLOVEHO NADHOZU
+   *
+   * Vysoke uceni technicke v Brne
+   * Fakulta informacnich technologii
+   * Ustav pocitacove grafiky a multimedii
+   *
+   * Autor:   Matej Nedela
+   * Vedouci: Ing. Tomas Milet, Ph.D.
+   */
+
+  /**
+   * @file
+   * @brief Field component
+   *
+   * @author Matej Nedela
+   */
+
+  import { T } from '@threlte/core';
+  import vertexShader from './shaders/vertex.glsl?raw';
+  import fragmentShader from './shaders/fragment.glsl?raw';
+
+  const { pitch } = $props();
+
+  const OUTFIELD_DISTANCE = 76.2;
+  const PITCHING_DISTANCE = 14.02;
+</script>
+
+<T.Mesh
+  position={[(OUTFIELD_DISTANCE - 8.23) / Math.sqrt(2), 0, 0]}
+  rotation.x={-Math.PI / 2}
+  rotation.z={-Math.PI / 4}
+>
+  <T.PlaneGeometry args={[OUTFIELD_DISTANCE + 8.23, OUTFIELD_DISTANCE + 8.23]} />
+  <T.ShaderMaterial
+    {vertexShader}
+    {fragmentShader}
+    uniforms={{
+      OUTFIELD_DISTANCE: { value: OUTFIELD_DISTANCE },
+      PITCHING_DISTANCE: { value: PITCHING_DISTANCE },
+      pitch: { value: pitch }
+    }}
+  />
+</T.Mesh>
