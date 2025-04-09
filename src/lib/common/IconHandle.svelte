9e190489a72294b6dfe26e659f6a01b9528f54c1
Matěj Neděla <nedela.matej@gmail.com>
2025-04-09 16:29:47 +0200

    feat: add `IconHandle` component

---
+++ src/lib/common/IconHandle.svelte
@@ -0,0 +1,26 @@
+<script>
+  /*
+   * Bakalarska prace
+   * MODELOVANI A ANALYZA TRAJEKTORII SOFTBALLOVEHO NADHOZU
+   *
+   * Vysoke uceni technicke v Brne
+   * Fakulta informacnich technologii
+   * Ustav pocitacove grafiky a multimedii
+   *
+   * Autor:   Matej Nedela
+   * Vedouci: Ing. Tomas Milet, Ph.D.
+   */
+
+  /**
+   * @file
+   * @brief Icon handle common component
+   *
+   * @author Matej Nedela
+   */
+</script>
+
+<span
+  class="tw-inline-flex tw-h-10 tw-w-10 tw-shrink-0 tw-cursor-move tw-items-center tw-justify-center tw-text-lg tw-text-neutral-600 tw-transition-[color] tw-duration-300 hover:tw-text-neutral-500"
+>
+  <i class="fa-solid fa-grip-vertical"></i>
+</span>
