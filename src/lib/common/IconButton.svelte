0014dbb088c41dd3b6865bfb1a49db7a9f6b1c09
Matěj Neděla <nedela.matej@gmail.com>
2025-04-09 16:29:42 +0200

    feat: add `IconButton` component

---
+++ src/lib/common/IconButton.svelte
@@ -0,0 +1,74 @@
+<script>
+  /*
+   * Bakalarska prace
+   * MODELOVANI A ANALYZA TRAJEKTORII SOFTBALLOVEHO NADHOZU
+   *
+   * Vysoke uceni technicke v Brne
+   * Fakulta informacnich technologii
+   * Ustav pocitacove grafiky a multimedii
+   *
+   * Autor:   Matej Nedela
+   * Vedouci: Ing. Tomas Milet, Ph.D.
+   */
+
+  /**
+   * @file
+   * @brief Icon button common component
+   *
+   * @author Matej Nedela
+   */
+
+  import Mousetrap from 'mousetrap';
+  import Tooltip from './Tooltip.svelte';
+  import { onDestroy, onMount } from 'svelte';
+  import { browser } from '$app/environment';
+
+  let {
+    icon, // fontawesome icon (e.g. 'fa-house')
+    smallIcon = undefined, // fontawesome icon (e.g. 'fa-house')
+    onclick,
+    tooltip = undefined,
+    shortcut = undefined, // mousetrap shortcut (e.g. 'mod+enter', 'mod+backspace', 'alt+a')
+    stopPropagation = false,
+    disabled = false,
+    cssClass = ''
+  } = $props();
+
+  onMount(() => {
+    if (shortcut && !disabled) {
+      Mousetrap.bind(shortcut, () => {
+        onclick();
+
+        return false;
+      });
+    }
+  });
+
+  onDestroy(() => {
+    if (browser && shortcut && !disabled) {
+      Mousetrap.unbind(shortcut);
+    }
+  });
+</script>
+
+<Tooltip value={tooltip} {shortcut}>
+  <button
+    onclick={(e) => {
+      if (stopPropagation) e.stopPropagation();
+      if (onclick) onclick();
+    }}
+    {disabled}
+    type="button"
+    class="{cssClass} relative inline-flex h-10 w-10 shrink-0 cursor-pointer items-center justify-center rounded-md border-0 bg-transparent text-lg text-orange-500 ring-2 ring-transparent transition-[box-shadow,color] duration-300 outline-none ring-inset hover:text-orange-400 focus-visible:ring-orange-500 disabled:cursor-not-allowed disabled:text-orange-300"
+    data-tooltip={tooltip}
+  >
+    <i class={icon}></i>
+
+    {#if smallIcon}
+      <i
+        style="-webkit-text-stroke: 0.04375rem #FFFFFF;"
+        class="{smallIcon} absolute right-1.5 bottom-1.5 text-sm leading-none"
+      ></i>
+    {/if}
+  </button>
+</Tooltip>
