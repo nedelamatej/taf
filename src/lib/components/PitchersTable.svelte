f2d212a860209731c1aae50aa3a36e61d959a8e5
Matěj Neděla <nedela.matej@gmail.com>
2025-05-02 19:07:13 +0200

    feat: add `PitchersTable` component

---
+++ src/lib/components/PitchersTable.svelte
@@ -0,0 +1,70 @@
+<script>
+  /*
+   * Bakalarska prace
+   * MODELOVANI A ANALYZA TRAJEKTORII SOFTBALLOVEHO NADHOZU
+   *
+   * Vysoke uceni technicke v Brne
+   * Fakulta informacnich technologii
+   * Ustav pocitacove grafiky a multimedii
+   *
+   * Autor:   Matej Nedela
+   * Vedouci: Ing. Tomas Milet, Ph.D.
+   */
+
+  /**
+   * @file
+   * @brief Pitchers table component
+   *
+   * @author Matej Nedela
+   */
+
+  import Table from '$lib/common/Table.svelte';
+
+  let { data } = $props();
+</script>
+
+<Table
+  {data}
+  fields={[
+    {
+      value: 'name',
+      label: 'Name',
+      type: 'text',
+      width: 3,
+      cssClass: 'font-bold'
+    },
+    {
+      value: 'date',
+      label: 'Date',
+      type: 'text',
+      width: 2,
+      cssClass: 'font-mono'
+    },
+    {
+      value: 'city',
+      label: 'City',
+      type: 'text',
+      width: 2
+    },
+    {
+      value: 'country',
+      label: 'Country',
+      type: 'text',
+      width: 2
+    },
+    {
+      value: 'Events',
+      type: 'buttonLink',
+      width: 1,
+      href: '/events/[page.params.id]/[value.id]'
+    },
+    {
+      value: 'Pitches',
+      type: 'buttonLink',
+      width: 2,
+      primary: true,
+      href: '/pitch/0/[value.id]/1'
+    }
+  ]}
+  csvFields={['id', 'organization', 'name', 'date', 'city', 'country']}
+/>
